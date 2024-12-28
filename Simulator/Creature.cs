e2f1be38593de4abcdafc0f7dd2b759514f70d12
Zofia Ostrowska <67557671+ZGabc@users.noreply.github.com>
2024-12-28 14:23:59 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,18 +1,42 @@
-﻿internal class Creature
+internal class Creature
 {
+    private string _name = "Unknown";
+    private int _level = 1;
+
     public string Name
     {
-        get;
-        set;
+        get => _name;
+        init
+        {
+            string trname = value.Trim();
+            if (trname.Length < 3)
+            {
+                trname = trname.PadRight(3, '#');
+            }
+            if (trname.Length > 25)
+            {
+                trname = trname.Substring(0, 25).TrimEnd();
+                if (trname.Length < 3)
+                {
+                    trname = trname.PadRight(3, '#');
+                }
+            }
+            if (char.IsLower(trname[0]))
+            {
+                trname = char.ToUpper(trname[0]) + trname.Substring(1);
+            }
+            _name = trname;
+        }
     }
     public int Level
     {
-        get;
-        set;
-    } = 3;
-    public Creature()
-    {
-    }
+        get => _level;
+        init
+        {
+            _level = Math.Max(1, Math.Min(10, value));
+        }
+    } 
+
     public Creature(string name, int level = 1)
     {
         Name = name;
@@ -26,4 +50,12 @@
     {
         Console.WriteLine($"Creature name:{Name} Creature level:[{Level}]");
     }
+    public void Upgrade()
+    {
+        if (_level < 10)
+        {
+            _level++;
+        }
+    }
+
 }
